27e312c15ea22989b37bf14bb4b164b6f8960116
arashkenji73 <kenjiarash@gmail.com>
2020-05-21 11:18:53 +0430

    loading animation subtitle & cta bug fixed

--- assets/js/loading.js
+++ assets/js/loading.js
@@ -5,8 +5,15 @@ function loading() {
     let svgName = document.getElementById('svg-name');
     
     
+    let bannerSubtitle = document.querySelector('#banner .subtitle');
+    
+    let bannerCta = document.querySelector('#banner .cta-wrapper');
+    
+    
     loading.classList.remove('active');
     svgName.dataset.animate = 'true';
+    bannerSubtitle.dataset.animate = 'true';
+    bannerCta.dataset.animate = 'true';
 }
 
 
