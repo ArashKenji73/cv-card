2797bfc0f3f4eb9bc87c8ea8b6678b7340236c44
arashkenji73 <kenjiarash@gmail.com>
2020-05-23 14:27:42 +0430

    fixed all module

--- assets/js/all.js
+++ assets/js/all.js
@@ -2,30 +2,12 @@ import {
     toggleMenu
 } from './toggle-menu.js';
 
-import {
-    addPercentToSkill
-} from './skills.js';
 
 import {
-    skillOffset,
-    skillHeight,
-    aboutOffset,
-    aboutHeight,
-    scrollFunction
+    scrollInit
 } from './scroll.js';
 
 
-function fadeIn(el) {
-    el.dataset.animate='true';
-}
+scrollInit();
 
-var elm = document.getElementById('about-me');
 
-//if scrolled to skills this function called
-window.onscroll = function () {
-    scrollFunction(fadeIn, elm, aboutOffset, aboutHeight);
-
-    
-    
-    scrollFunction(addPercentToSkill, elm ,skillOffset, skillHeight);
-};
--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -10,10 +10,26 @@ let
 //get skills scroll position
 skillOffset = document.getElementById('skills').offsetTop;
 
+//import from skills
+
+
+
+import {
+    addPercentToSkill
+} from './skills.js';
+
+
+
+
+
+
+
+
+
+
+
 //get skills height
 skillHeight = document.getElementById('skills').offsetHeight;
-
-
 
 
 
@@ -28,17 +44,33 @@ aboutHeight = document.getElementById('about-me').offsetHeight;
 
 
 //call callback after scroll to point
-function scrollFunction(customCallback, element='', offset, height) {
+function scrollFunction(customCallback, element = '', offset, height) {
     if (document.documentElement.scrollTop > (offset - height) - 60) {
         customCallback(element);
     }
 }
 
 
+
+var aboutSec = document.getElementById('about-me');
+
+
+function fadeIn(el) {
+    el.dataset.animate='true';
+}
+
+//if scrolled to skills this function called
+function scrollInit() {
+    window.onscroll = function () {
+        //fadeIn element
+        scrollFunction(fadeIn, aboutSec, aboutOffset, aboutHeight);
+
+        scrollFunction(addPercentToSkill, '', skillOffset, skillHeight);
+    };
+}
+
+
+
 export {
-    skillOffset,
-    skillHeight,
-    aboutOffset,
-    aboutHeight,
-    scrollFunction
+    scrollInit
 }
